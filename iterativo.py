9e828d19dfffdb14750108459cd8eb03ba3334ca
frankguido <ffelixg@ulasalle.edu.pe>
2022-05-13 01:17:29 -0500

    iterativo con tiempo

--- iterativo.py
+++ iterativo.py
@@ -1,3 +1,5 @@
+from timeit import default_timer
+
 def iterativo(n):
   terminos = [0,1]
   i = 2
@@ -5,3 +7,11 @@ def iterativo(n):
     terminos.append(terminos[i-1]+terminos[i-2])
     i = i+1
   return terminos[n]
+
+
+inicio = default_timer()
+iterativo(34)
+fin = default_timer()
+print(fin - inicio)
+
+print(iterativo(34))
