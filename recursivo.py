b99079622aaaac68513a0f0c83fb89d146ee0159
frankguido <ffelixg@ulasalle.edu.pe>
2022-05-13 01:19:05 -0500

    RECURSIVO con tiempo

--- recursivo.py
+++ recursivo.py
@@ -1,6 +1,16 @@
+
 def fib(n):
     if n == 0:
         return 0
     if n == 1:
         return 1
     return fib(n-1) + fib(n-2)
+
+
+
+inicio = default_timer()
+fib(34)
+fin = default_timer()
+print(fin - inicio)
+
+print (fib(34))
